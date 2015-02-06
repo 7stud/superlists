39fe9a39cb295b4683adf04dc63971f8c2fdd215
7stud <7stud@excite.com>
2015-02-06 01:50:55 -0700

    Add more FT's that check inputting a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -9,25 +10,39 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_root_url_goes_to_home_page_view(self):
+    def test_can_create_a_list_and_retrieve_it_later(self):
         #Erin visits the website:
         self.browser.get('http://localhost:8000')
-        #She notices that the title has 'To-Do' in it:
+        #She notices that the title and header have 'To-Do' in them:
         self.assertIn('To-Do lists', self.browser.title)
+        #self.fail("Finish the tests!")
+        header = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do lists', header)
+
+
+        #There is a text box to begin a todo list:
+        textfield = self.browser.find_element_by_id('new_item')
+        self.assertEqual(
+            textfield.get_attribute('placeholder'), 
+            'Enter a to-do item',
+        )
+
+        #She fills in the text box with 'Buy peacock feathers':
+        textfield.send_keys('Buy peacock feathers')
+
+        #She hits Enter and sees the page update with a list that
+        #says '#1 Buy peacock feathers'.
+        textfield.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('list')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        #Erin notices there is another textbox displayed. She enters 
+        #the next item in her list: 'Tie fly using peackock feathers'.
         self.fail("Finish the tests!")
-
-
-
-#There is a text box to begin a todo list:
-
-#She fills in the text box with 'Buy peacock feathers':
-
-#She hits Enter and sees the page update with a list that
-#says
-#'#1 Buy peacock feathers', and there is another textbox.
-
-#Erin enters the next item in her list: 'Tie fly using 
-#peackock feathers'.
 
 #She wonders if she can retrieve the list when 
 #she comes back to the website:
